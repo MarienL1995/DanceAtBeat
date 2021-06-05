a655317a0866fa4ecec87a11b55e675b04de1faa
MarienL1995 <lander.marien@vdabcampus.be>
2021-06-05 15:51:26 +0200

    first commit

---
+++ src/test/java/com/danceatbeat/danceatbeat/DanceatbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danceatbeat.danceatbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceatbeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
